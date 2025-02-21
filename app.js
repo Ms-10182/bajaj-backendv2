ebee4d2cc04bab97601efb7787715e382744edf0
Mayank Sharma <22bcs10182@cuchd.in>
2025-02-21 18:22:57 +0530

    update code

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(cors({
 app.use(express.json({limit:"16kb"}));
 app.use(urlencoded({ extended: true })); 
 
-import router from './routes/user.routes.js';
+import router from './src/routes/user.routes.js';
 
 app.use("/api/v1/",router);
 
